e33899b1ed81c09a3fb1e6f2e329b8c6282c6b05
Vineeth <vineethkumar1607@gmail.com>
2025-10-30 00:10:44 +0530

    chnaged the button name

--- netflix-ai-assistance/src/components/Header.jsx
+++ netflix-ai-assistance/src/components/Header.jsx
@@ -90,7 +90,7 @@ const Header = () => {
                                 onClick={handleGptModelToggle}
                                 data-dialog-target="animated-modal"
                                 className="rounded-md bg-red-800 py-2 px-4 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-red-700 focus:shadow-none active:bg-red-700 hover:bg-red-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none ml-2" type="button">
-                                Ask GPT
+                                AI Search
                             </button>
                             {showGptModal && <AiAssistant />}
 
